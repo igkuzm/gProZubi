0bc274f97cc9b94f57d6332d229c170b7f3f0c7a
Igor V. Sementsov <ig.kuzm@gmail.com>
2023-04-01 05:59:12 +1000

    init

---
+++ src/callbacks.c
@@ -0,0 +1,42 @@
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#include <gtk/gtk.h>
+
+#include "callbacks.h"
+#include "interface.h"
+#include "support.h"
+
+
+void
+on_patientListButton_activate          (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_doctorListButton_activate           (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_priceListButton_activate            (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
+
+void
+on_configButton_activate               (GtkButton       *button,
+                                        gpointer         user_data)
+{
+
+}
+
---
+++ src/callbacks.h
@@ -0,0 +1,18 @@
+#include <gtk/gtk.h>
+
+
+void
+on_patientListButton_activate          (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_doctorListButton_activate           (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_priceListButton_activate            (GtkButton       *button,
+                                        gpointer         user_data);
+
+void
+on_configButton_activate               (GtkButton       *button,
+                                        gpointer         user_data);
